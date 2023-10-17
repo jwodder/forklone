6f171f863afa6718224ecb4725230d0c27de217f
John T. Wodder II <git@varonathe.org>
2023-10-17 16:11:31 -0400

    Packaging

--- forklone.py
+++ forklone.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python3
-# The MIT License (MIT)
-#
-# Copyright (c) 2020-2021, 2023 John Thorvald Wodder II
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 """
 forklone.py — Fork & clone a GitHub repository
 ==============================================
@@ -63,19 +40,10 @@ via the ``GH_TOKEN`` or ``GITHUB_TOKEN`` environment variable (possibly in an
 setting the ``hub.oauthtoken`` Git config option.
 """
 
-__requires__ = [
-    "click >= 7.0",
-    "ghrepo ~= 0.1",
-    "ghtoken ~= 0.1",
-    "PyGithub ~= 1.53",
-]
-
-__requires_python__ = ">= 3.6"
-
 __author__       = 'John Thorvald Wodder II'
 __author_email__ = 'forklone@varonathe.org'
 __license__      = 'MIT'
-__url__          = 'https://gist.github.com/cee837802578a4fc8854df60529af98c'
+__url__          = 'https://github.com/jwodder/forklone'
 
 import os
 from   shlex  import split
